79255b00169cf143aa3556133ada685a58401ed5
aravin.siva <aravinarjunn@gmail.com>
2018-09-28 22:55:41 -0400

    Initial Commit

---
+++ Dicee App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee App
+//
+//  Created by Aravin Sivakumar on 2018-09-28.
+//  Copyright © 2018 Aravin Sivakumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
